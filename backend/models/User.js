2a1c074212dbaeabac6e1a4d4f7d81be1d5d266a
Ahemad <ahemadbakali26@gmail.com>
2025-06-28 11:26:11 +0530

    "_id change not allowed mongodb"

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,12 +2,6 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 
-    _id: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-
     name: {
         type: String,
         required: true,
