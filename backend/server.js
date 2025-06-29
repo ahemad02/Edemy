cec6e6187492a7305b71b6c3d4445f5581cce5f3
Ahemad <ahemadbakali26@gmail.com>
2025-06-30 00:29:13 +0530

    abcd

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ app.get("/", (req, res) => {
 app.post("/clerk", express.json(), clerkWebhooks);
 
 app.use("/api/educator", express.json(), educatorRouter);
-app.use("/api/course", express.json(), courseRouter)
-app.use("/api/user", express.json(), userRouter)
+app.use("/api/course", express.json(), courseRouter);
+app.use("/api/user", express.json(), userRouter);
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
 
