601c5d29e193b44888036df395a835c7c63a2c49
Ahemad <ahemadbakali26@gmail.com>
2025-06-29 12:29:44 +0530

    "express json"

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 app.use(clerkMiddleware())
 
 // Routes
